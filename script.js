05f1e450e951fefab1ad9c5a670633064806faf1
rutujaarage <rutujaarage99@gmail.com>
2022-03-30 17:16:42 +0530

    loops

---
+++ script.js
@@ -0,0 +1,40 @@
+
+// .........................................for loops.............................
+
+// let i=0;
+// for(;i<10;){
+//     console.log("value of i is",i);
+//     i++;
+// }
+
+
+
+//..........................................while loops........................
+
+// let i=0;
+// while(i<10){
+//     console.log("valueof i",i );
+//     i++;
+// }
+// console.log("outside loop");
+
+
+//......................................... do while loop..................................
+
+// let i=0;
+// do{
+//    console.log("value of i is :",i);
+//    i++;
+// }
+// while(i<10)
+
+//........................................ nested loops............................
+// it happen in only for loop
+// for loop  is master of all loops , they having Iniliazation, condition , declaration totaly clear either of while and do-while.
+
+// for(i=0;i<10;i++){
+//     for(j=1;j<5;j++){
+//         console.log("value of i is:",i , "and j is:",j);
+//     }
+// }
+
